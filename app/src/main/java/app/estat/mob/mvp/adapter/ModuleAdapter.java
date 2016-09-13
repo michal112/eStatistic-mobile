11305fc917ff94fb40e90b13b48862656c5d3207
Michal Szubert <mszubert@itti.com.pl>
2016-09-13 10:03:21 +0200

    zmiany

--- app/src/main/java/app/estat/mob/mvp/adapter/ModuleAdapter.java
+++ app/src/main/java/app/estat/mob/mvp/adapter/ModuleAdapter.java
@@ -20,33 +20,52 @@ import app.estat.mob.mvp.util.ViewUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ModuleAdapter extends RecyclerView.Adapter<ModuleAdapter.ModuleViewHolder> {
-    private Context mContext;
+public class ModuleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final static int HEADER = 0;
+
+    private final static int MODULE = 1;
+
+    private final Context mContext;
 
     private final List<Module> mModules;
 
     public ModuleAdapter(List<Module> modules, Context context) {
         this.mModules = modules;
-        mContext = context;
+        this.mContext = context;
     }
 
     @Override
-    public ModuleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(
-                R.layout.fragment_dashboard_item, parent, false);
-        return new ModuleViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch(viewType) {
+            case HEADER:
+                return new HeaderHolder(LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.recycler_view_header, parent, false));
+            case MODULE:
+                return new ModuleViewHolder(LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.fragment_dashboard_item, parent, false));
+            default:
+                return null;
+        }
     }
 
     @Override
-    public void onBindViewHolder(ModuleViewHolder holder, int position) {
-        holder.bindName(ViewUtils.getResId(mContext, mModules.get(position % 4).getNameRes()));
-        holder.bindDescription(ViewUtils.getResId(mContext, mModules.get(position % 4).getDescriptionRes()));
-        holder.bindImage(ViewUtils.getResId(mContext, mModules.get(position % 4).getIconRes()));
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ModuleViewHolder) {
+            ModuleViewHolder moduleViewHolder = ((ModuleViewHolder) holder);
+            moduleViewHolder.bindName(ViewUtils.getResId(mContext, mModules.get(position % 4).getNameRes()));
+            moduleViewHolder.bindDescription(ViewUtils.getResId(mContext, mModules.get(position % 4).getDescriptionRes()));
+            moduleViewHolder.bindImage(ViewUtils.getResId(mContext, mModules.get(position % 4).getIconRes()));
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position > 0 ? MODULE : HEADER;
     }
 
     @Override
     public int getItemCount() {
-        return mModules.size() * 4;
+        return mModules.size() * 4 + 1;
     }
 
     public static class ModuleViewHolder extends RecyclerView.ViewHolder {
@@ -75,6 +94,12 @@ public class ModuleAdapter extends RecyclerView.Adapter<ModuleAdapter.ModuleView
 
         public void bindImage(@DrawableRes int resId) {
            mImage.setImageResource(resId);
+        }
+    }
+
+    public static class HeaderHolder extends RecyclerView.ViewHolder {
+        public HeaderHolder(View itemView) {
+            super(itemView);
         }
     }
 }
